9b06db1d719f8c31ca13dd4d36de0d387b1d4caa
George Dimas <georgedimas333@gmail.com>
2026-07-18 21:42:35 -0500

    new results

--- qiskit_script.py
+++ qiskit_script.py
@@ -1,10 +1,9 @@
 # ENTER YOUR PREFERRED TRAINING PARAMETERS HERE 
 
 # training sizes
-TRAIN_EPISODES_FULL = 1000 # <-- CHANGE THIS FOR SIMULATION TRAINING SESSIONS
+TRAIN_EPISODES_FULL = 5000 # <-- CHANGE THIS FOR SIMULATION TRAINING SESSIONS
 POWER_SYSTEM = "matpower/case300/2017-01-01"  # <-- CHANGE THIS TO YOUR PREFERRED TEST INSTANCE	
 
-# Quantum evaluation parameters (keeps jobs short)
+# Quantum evaluation parameters 
 QPU_EVAL_EPISODES = 1      # small number of episodes to evaluate
 QPU_EVAL_SHOTS = 512       # shots per circuit submission for marginals
-# ----------------------------
